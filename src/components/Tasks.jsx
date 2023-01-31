a24da1f3f745c5a7de4b8c8b428ed91d9bafddb9
unknown <nourmostafa01551@gmail.com>
2023-01-31 15:31:40 +0200

    Empty textbox after adding task,Disable add button when input field be empty

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,14 +6,16 @@ import { useParams } from "react-router-dom";
 const Tasks = () => {
   const [arr,setArray] = useState([]);
   const [InputData, setInputData] = useState([]);
-
+  const [isDisabled, setDisabled] = useState(false);
     const handleSubmit=(e)=>{
         e.preventDefault();
+        setDisabled(true);
         
     }
     const addToArray=()=>{
       arr.push(InputData);
       setArray([...arr])
+      setInputData([""]);
     }
   return (
     <div className="tasks">
@@ -22,7 +24,7 @@ const Tasks = () => {
         <div className="content">
           <form className='todo-form' onSubmit={handleSubmit}>
                 <input type="text" onChange={(e)=>setInputData(e.target.value)}  value={InputData} placeholder='Write a Task....'name='text' className='todo-input' />
-                <button className='todo-button' onClick={addToArray}>Add Task</button>
+                <button className='todo-button' disabled={isDisabled} onClick={addToArray}>Add Task</button>
             </form>
         </div>
     </div>
