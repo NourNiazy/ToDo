48d3707bd3b9147f441a99a7b7f1bcec93a687a3
unknown <nourmostafa01551@gmail.com>
2023-02-01 03:59:33 +0200

    fix disable button

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,25 +6,16 @@ import { useParams } from "react-router-dom";
 const Tasks = () => {
   const [arr,setArray] = useState([]);
   const [InputData, setInputData] = useState([]);
-  const [isDisabled, setDisabled] = useState(true);
     const handleSubmit=(e)=>{
         e.preventDefault();
         
         
     }
     const addToArray=()=>{
-      if(InputData==""){
-        console.log("empty");
-      }
-      else {
-        
       arr.push(InputData);
       setArray([...arr])
       setInputData([""]);
-      }
-     
       
-
     }
   return (
     <div className="tasks">
@@ -33,7 +24,7 @@ const Tasks = () => {
         <div className="content">
           <form className='todo-form' onSubmit={handleSubmit}>
                 <input type="text" onChange={(e)=>setInputData(e.target.value)}  value={InputData} placeholder='Write a Task....'name='text' className='todo-input' />
-                <button className='todo-button'  onClick={addToArray}>Add Task</button>
+                <button className='todo-button' onClick={addToArray} disabled={InputData==""?true:false}>Add Task</button>
             </form>
         </div>
     </div>
