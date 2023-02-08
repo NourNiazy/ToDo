e11d2a16bc83e98baca53c493e8086da37ec552d
Islam Mansour <eslam.mansour15@gmail.com>
2023-02-08 16:35:53 +0200

    switch done status of task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Tasks from './Tasks';
-const Task = ({arr,deleteTask}) => { 
+const Task = ({arr,deleteTask, changeTaskStatus}) => { 
   return (
     <div>
-      <div className="tasks-box">
+      <div className="tasks-box" >
         <div className="container">
           <div className="content">
             {arr.map((task)=>(  
-              <div className="task-box"key={task.id} id={task.id}>
-                <p >{task.taskContent}</p>
+              <div className="task-box"key={task.id} id={task.id} onDoubleClick={()=>changeTaskStatus(task)}>
+                <p className={task.isDone?"complete":"not-complete"}>{task.taskContent}</p>
                 <button onClick={() => deleteTask(task.id)} className="delete-btn">Delete</button>
               </div>
             ))}
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -11,25 +11,49 @@ const Tasks = () => {
     }
     const addToArray=(task)=>{
       
-     let taskObject={
-    taskContent:task,
-    id:arr.length+1
-  } 
+      let taskObject = {
+        taskContent:task,
+        id:arr.length+1,
+        isDone: false
+      } 
       // add task to array
       arr.push(taskObject);
 
       // update array
       setArray([...arr])
     }
+
+
+    
     const deleteTask=(id)=>{
-    const newarr = arr.filter((task) => task.id !== id);
+      const newarr = arr.filter((task) => task.id !== id);
       setArray([...newarr]);
+    }
+
+
+    const changeTaskStatus = (t) => {
+
+      // copy arr in `indexOfTask`
+      let currnetArr = arr
+
+      // get index of t
+      const indexOfTask = arr.findIndex((task) => task.id == t.id);
+
+      // change isDone
+      t.isDone = !t.isDone
+
+      // update task in `currnetArr` array in index `indexOfTask`
+      currnetArr[indexOfTask] = t
+
+      // update new array with `indexOfTask`
+      setArray([...currnetArr]);
+    
     }
     
   return (
     <div className="tasks">
       <TaskForm handleSubmit={handleSubmit} addToArray={addToArray}/>
-      <Task arr={arr} deleteTask={deleteTask}/>
+      <Task arr={arr} deleteTask={deleteTask} changeTaskStatus={changeTaskStatus}/>
     
     </div>
     
